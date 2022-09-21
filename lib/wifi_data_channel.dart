0b0a8710de8989e6619dd689f04739e745c1fb87
Rémy Raes <contact@remyraes.com>
2022-09-21 15:52:53 +0200

    feat: initSender opens Wi-Fi access point

--- lib/wifi_data_channel.dart
+++ lib/wifi_data_channel.dart
@@ -1,9 +1,13 @@
 library wifi_data_channel;
 
+import 'dart:io';
+
 import 'package:channel_multiplexed_scheduler/channels/abstractions/bootstrap_channel.dart';
 import 'package:channel_multiplexed_scheduler/channels/abstractions/data_channel.dart';
 import 'package:channel_multiplexed_scheduler/channels/channel_metadata.dart';
 import 'package:channel_multiplexed_scheduler/file/file_chunk.dart';
+import 'package:flutter/foundation.dart';
+import 'package:wifi_iot/wifi_iot.dart';
 
 
 class WifiDataChannel extends DataChannel {
@@ -16,9 +20,12 @@ class WifiDataChannel extends DataChannel {
   }
 
   @override
-  Future<void> initSender(BootstrapChannel channel) {
-    // TODO: implement initSender
-    throw UnimplementedError();
+  Future<void> initSender(BootstrapChannel channel) async {
+    await WiFiForIoTPlugin.setWiFiAPEnabled(true);
+    debugPrint("[WifiChannel] Sender successfully initialized.");
+    debugPrint("[WifiChannel]     IP: ${await WiFiForIoTPlugin.getIP()}");
+    debugPrint("[WifiChannel]     SSID: ${await WiFiForIoTPlugin.getWiFiAPSSID()}");
+    debugPrint("[WifiChannel]     Key: ${await WiFiForIoTPlugin.getWiFiAPPreSharedKey()}");
   }
 
   @override
