98a5c6c248317a85205ca534f5bf7c819284ba2d
Rémy Raes <contact@remyraes.com>
2023-10-04 16:05:57 +0200

    test: retrieveHotspotIPAddress should not find address without second parameter

--- test/address_test.dart
+++ test/address_test.dart
@@ -31,5 +31,16 @@ void main() {
       InternetAddress result = canal.retrieveHotspotIPAddress([ni1, hotspot, ni2], [ni1, ni2]);
       expect(result.address, hotspotAddress);
     });
+
+    test('should not find address without second parameter', () {
+      String hotspotAddress = '192.168.53.71';
+      NetworkInterface ni1 = TestNetworkInterface("wlan0", ['10.201.0.146']);
+      NetworkInterface ni2 = TestNetworkInterface("v4-rmnet_data2", ['192.0.0.4']);
+      NetworkInterface hotspot = TestNetworkInterface("wlan", [hotspotAddress]);
+
+      expect(() => canal.retrieveHotspotIPAddress([ni1, hotspot, ni2], null),
+          throwsA(predicate((e) => e is StateError
+              && e.message == 'Could not retrieve hotspot IP address from provided information.')));
+    });
   });
 }
