a98165e0302d950367e238be4afffb6eb5ce3391
Remy Raes <contact@remyraes.com>
2023-11-24 15:58:56 +0100

    refactor: export retrieveHotspotIPAddress method to dedicated package

--- lib/wifi_data_channel.dart
+++ lib/wifi_data_channel.dart
@@ -8,6 +8,7 @@ import 'package:venice_core/metadata/channel_metadata.dart';
 import 'package:venice_core/channels/events/data_channel_event.dart';
 import 'package:flutter/foundation.dart';
 import 'package:venice_core/network/message.dart';
+import 'package:wifi_data_channel/access_point_utils.dart';
 import 'package:wifi_iot/wifi_iot.dart';
 import 'package:wifi_scan/wifi_scan.dart';
 
@@ -161,77 +162,6 @@ class WifiDataChannel extends DataChannel {
   @override
   Future<void> sendMessage(VeniceMessage chunk) async {
     client!.write(chunk.toBytes());
-  }
-
-  /// Returns the Wi-Fi hotspot IP address of the current device.
-  ///
-  /// This methods accepts a [oldInterfaces] list of interfaces that was saved
-  /// in memory *before* turning the Wi-Fi hotspot on, supposedly helping in
-  /// finding the newly created network interface.
-  ///
-  /// This will throw an error if the [oldInterfaces] argument is not provided
-  /// and several private class IPs are present in [interfaces].
-  InternetAddress retrieveHotspotIPAddress(List<NetworkInterface> interfaces,
-      {List<NetworkInterface>? oldInterfaces}) {
-    if (interfaces.isEmpty) {
-      throw ArgumentError(
-          'Cannot retrieve hotspot address from an empty interfaces list.');
-    }
-
-    // Debug log
-    debugPrint("Input interfaces: $interfaces");
-    if (oldInterfaces != null) {
-      debugPrint("Input old interfaces: $oldInterfaces");
-    } else {
-      debugPrint("No old interfaces provided.");
-    }
-
-    // Retrieve all private addresses
-    List<InternetAddress> privateAddresses = [];
-    for (NetworkInterface ni in interfaces) {
-      privateAddresses.addAll(ni.addresses.where((address) {
-        List<String> words = address.address.split(".");
-
-        // Class A
-        if (words[0] == "10") return true;
-        // Class B
-        int second = int.parse(words[1]);
-        if (words[0] == "172" && second >= 16 && second <= 31) return true;
-        // Class C
-        if (words[0] == "192" && words[1] == "168") return true;
-
-        return false;
-      }).toList());
-    }
-
-    // If there's only one private address, return it
-    if (privateAddresses.length == 1) {
-      return privateAddresses.first;
-    }
-    // Throw if there are no private addresses
-    if (privateAddresses.isEmpty) {
-      throw StateError('No private address was found.');
-    }
-
-    // If there are several private interfaces, we need to use [oldInterfaces]
-    // list to find out which one appeared when the hotspot was turned on.
-    if (oldInterfaces != null) {
-      List<InternetAddress> matching = privateAddresses.where((address) {
-        return oldInterfaces.where((interface) {
-          return interface.addresses.contains(address);
-        }).isEmpty;
-      }).toList();
-
-      if (matching.length == 1) {
-        return matching.first;
-      } else {
-        throw StateError(
-            'Several compatible private IP addresses appeared, cannot choose between them.\nPotential candidates: $matching');
-      }
-    }
-
-    throw StateError(
-        "Could not retrieve hotspot IP address from provided information.");
   }
 
   @override
--- test/address_test.dart
+++ test/address_test.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:wifi_data_channel/access_point_utils.dart';
 import 'package:wifi_data_channel/wifi_data_channel.dart';
 
 class TestNetworkInterface extends NetworkInterface {
